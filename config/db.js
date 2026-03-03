289b2a4b03f45e8c59f8186da13b2dce891411a1
883xd <atichatsaengmanee@gmail.com>
2026-03-03 15:32:42 +0700

    add try catch to db

--- config/db.js
+++ config/db.js
@@ -1,16 +1,16 @@
 const mongoose = require('mongoose');
 
 const connectDB = async () => {
-    mongoose.set('strictQuery', true);
-    const conn = await mongoose.connect(process.env.MONGO_URI);
-    // Old version
-    // const conn = await mongoose.connect(process.env.MONGO_URI, {
-    //     useNewUrlParser: true,
-    //     userUnifiedTopology: true
-    // });
+    try {
+        mongoose.set('strictQuery', true);
 
-    console.log(`MongoDB Connected: ${conn.connection.host}`);
-}
+        const conn = await mongoose.connect(process.env.MONGO_URI);
 
+        console.log("MongoDB Connected: ${conn.connection.host}");
+    } catch (error) {
+        console.error('Error connecting to MongoDB:', error.message);
+        process.exit(1);
+    }
+};
 
 module.exports = connectDB;
